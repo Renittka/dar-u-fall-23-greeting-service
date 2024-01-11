21be1248641d9cd021756e42f0308097e0cc1c92
Anna Ogorodova <aogorodova@dar.io>
2024-01-11 17:41:50 +0100

    Initial commit

---
+++ src/main/java/kz/dar/university/greetingservice/controller/GreetingController.java
@@ -0,0 +1,44 @@
+package kz.dar.university.greetingservice.controller;
+
+import kz.dar.university.greetingservice.domain.Greeting;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+
+    @GetMapping
+    public Greeting getGreeting(
+            @RequestParam(required = false, defaultValue = "World") String name,
+            @RequestParam(required = false, defaultValue = "World") String surname
+    ) {
+        log.info("Name: " + name);
+        log.info("Surname: " + surname);
+        log.info("getGreeting with request param");
+        return new Greeting(String.format(TEMPLATE, name));
+    }
+
+    /*
+    localhost:8080/users/123
+    localhost:8080/products/category/{phone}?sort=price&color=black
+     */
+
+    @GetMapping("/{name}/{second}")
+    public Greeting getGreetingPathVariable(
+            @PathVariable String name,
+            @PathVariable String second,
+            @RequestParam(required = false, defaultValue = "World") String middleName,
+            @RequestParam(required = false, defaultValue = "World") String surname
+    ) {
+        log.info("Name: " + name);
+        log.info("Request param 1: " + middleName);
+        log.info("Request param 2: " + surname);
+        log.info("getGreeting with path variable");
+        return new Greeting(String.format(TEMPLATE, name));
+    }
+
+}
---
+++ src/test/java/kz/dar/university/greetingservice/GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dar.university.greetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
